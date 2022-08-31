eee7ad67f5ef7b7eed5d8c758ca2251ceb4879b5
Sevenpros <sethbisoso@gmail.com>
2022-08-31 07:30:50 +0200

    Fix no display Problem

--- src/components/details.js
+++ src/components/details.js
@@ -1,19 +1,50 @@
-import {
-  Routes,
-  Route,
-} from 'react-router-dom';
-import HomePage from './HomePage';
-// eslint-disable-next-line
-  import Details from './details';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 
-const Router = () => (
-  <>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/details" element={<Details />} />
-    </Routes>
-  </>
+const Details = () => {
+  const details = useSelector((state) => state.countryReducer.details);
+  return (
+    <>
+      <div className="navigation">
+        <Link to="/" className="arrow">
+          <FontAwesomeIcon icon={faAngleLeft} />
+        </Link>
+      </div>
+      <div className="headline">
 
-);
-
-export default Router;
+        <h1>
+          {details.Country}
+        </h1>
+      </div>
+      <ul>
+        <li className="c-details">
+          <span>Total Cases Confirmed:</span>
+          {details.TotalConfirmed}
+        </li>
+        <li className="c-details">
+          <span>Total Deaths Confirmed:</span>
+          {details.TotalDeaths}
+        </li>
+        <li className="c-details">
+          <span>Total Recovered:</span>
+          {details.TotalRecovered}
+        </li>
+        <li className="c-details">
+          <span>New Case Confirmed:</span>
+          {details.NewConfirmed}
+        </li>
+        <li className="c-details">
+          <span>New Recovered:</span>
+          {details.NewRecovered}
+        </li>
+        <li className="c-details">
+          <span>New Deaths :</span>
+          {details.NewDeaths}
+        </li>
+      </ul>
+    </>
+  );
+};
+export default Details;
